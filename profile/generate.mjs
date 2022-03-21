24dc27675f8900090b35eb428812e04224757719
Jordan Harband <ljharb@gmail.com>
2022-03-20 22:22:39 -0700

    profile/generate: add pagination, fix whitespace

--- profile/generate.mjs
+++ profile/generate.mjs
@@ -9,83 +9,98 @@ const token = process.env.GH_TOKEN;
 
 const org = 'inspect-js';
 
-const response = await graphql(
+async function getResponse(cursor = '') {
 	// https://stackoverflow.com/a/51412084/632724
-	`
+	return graphql(
+		`
 		{
-			search(query: "org:${org}", type: REPOSITORY, first: 100) {
-			repositoryCount
-			edges {
-				node {
-                    ... on Repository {
-                        nameWithOwner,
-                        isPrivate,
-                        object(expression: "HEAD:package.json") {
-                            ... on Blob {
-                                text
-                            }
-                        }
-                    }
+			search(query: "org:${org}", type: REPOSITORY, first: 100 ${cursor ? `after:${JSON.stringify(cursor)}` : ''}) {
+				pageInfo {
+					hasNextPage
+					endCursor
+				}
+				repositoryCount
+				edges {
+					node {
+						... on Repository {
+							nameWithOwner,
+							isPrivate,
+							object(expression: "HEAD:package.json") {
+								... on Blob {
+									text
+								}
+							}
+						}
+					}
 				}
 			}
-			}
 		}
-	`,
-	{
-		headers: {
-			authorization: `token ${token}`
+		`,
+		{
+			headers: {
+				authorization: `token ${token}`
+			},
 		},
-	},
-);
-
-async function getPackageData(repo) {
-    // get package.json from repo
-    return {
-        name,
-        description,
-    }
+	);
 }
 
-const repos = response.search.edges.map(x => ({
-    repo: x.node.nameWithOwner.slice(org.length + 1),
-    pkg: x.node.object ? JSON.parse(x.node.object.text) : undefined,
+async function getPackageData(repo) {
+	// get package.json from repo
+	return {
+		name,
+		description,
+	}
+}
+
+const edges = [];
+let hasNextPage = true;
+let cursor = '';
+while (hasNextPage) {
+	const response = await getResponse(cursor);
+	({ hasNextPage, endCursor: cursor } = response.search.pageInfo);
+	edges.push(...response.search.edges);
+}
+
+const repos = edges.map(x => ({
+	repo: x.node.nameWithOwner.slice(org.length + 1),
+	pkg: x.node.object ? JSON.parse(x.node.object.text) : undefined,
 }))
 .filter(({ repo: x }) => !x.startsWith('.'))
 .sort();
 
 const pkgs = {
-    predicates: [],
-    comparisons: [],
-    environment: [],
-    describe: [],
-    unknown: [],
+	predicates: [],
+	comparisons: [],
+	environment: [],
+	describe: [],
+	unknown: [],
 };
 function sortRepo(a, b) {
-    return a.repo.localeCompare(b.repo);
+	return a.repo.localeCompare(b.repo);
 }
 repos.forEach((x) => {
-    if (x.repo.endsWith('-equal') || x.repo.endsWith('-is')) {
-        pkgs.comparisons.push(x);
-        pkgs.comparisons.sort(sortRepo);
-    } else if (x.repo.startsWith('has-') || x.repo.startsWith('available-') || x.repo.includes('-have-') || x.repo.includes('supports-')) {
-        pkgs.environment.push(x);
-        pkgs.environment.sort(sortRepo);
-    } else if (x.repo.startsWith('which-') || x.repo.startsWith('get-') || x.repo.includes('inspect') || x.repo.endsWith('-length')) {
-        pkgs.describe.push(x);
-        pkgs.describe.sort(sortRepo);
-    } else if (x.repo.startsWith('is-')) {
-        pkgs.predicates.push(x);
-        pkgs.predicates.sort(sortRepo);
-    } else {
-        pkgs.unknown.push(x);
-        pkgs.unknown.sort(sortRepo);
-    }
+	if (x.repo.endsWith('-equal') || x.repo.endsWith('-is')) {
+		pkgs.comparisons.push(x);
+		pkgs.comparisons.sort(sortRepo);
+	} else if (x.repo.startsWith('has-') || x.repo.startsWith('available-') || x.repo.includes('-have-') || x.repo.includes('supports-') || x.repo.endsWith('-info')) {
+		pkgs.environment.push(x);
+		pkgs.environment.sort(sortRepo);
+	} else if (x.repo.startsWith('which-') || x.repo.startsWith('get-') || x.repo.includes('inspect') || x.repo.endsWith('-length')) {
+		pkgs.describe.push(x);
+		pkgs.describe.sort(sortRepo);
+	} else if (x.repo.startsWith('is-')) {
+		pkgs.predicates.push(x);
+		pkgs.predicates.sort(sortRepo);
+	} else {
+		pkgs.unknown.push(x);
+		pkgs.unknown.sort(sortRepo);
+	}
 });
 
 console.log(pkgs);
 
 if (pkgs.unknown.length > 0) {
-    throw `unknown packages found!: ${pkgs.unknown.map(x => x.repo).join(', ')}`;
+	throw `unknown packages found!: ${pkgs.unknown.map(x => x.repo).join(', ')}`;
 }
 delete pkgs.unknown;
 
@@ -94,33 +109,33 @@ This github and [npm](https://npmjs.com/~${org}) organization houses JavaScript 
 `;
 
 const meta = {
-    predicates: {
-        title: 'Predicates',
-        description: 'Functions that ask a question and produce a yes or no answer (`true` or `false`).',
-    },
-    comparisons: {
-        title: 'Comparisons',
-        description: 'Packages that compare two things.',
-    },
-    environment: {
-        title: 'Environment',
-        description: 'Packages that tell you about the JS env.',
-    },
-    describe: {
-        title: 'Describe',
-        description: 'Packages that tell you about a specific JS value.',
-    },
+	predicates: {
+		title: 'Predicates',
+		description: 'Functions that ask a question and produce a yes or no answer (`true` or `false`).',
+	},
+	comparisons: {
+		title: 'Comparisons',
+		description: 'Packages that compare two things.',
+	},
+	environment: {
+		title: 'Environment',
+		description: 'Packages that tell you about the JS env.',
+	},
+	describe: {
+		title: 'Describe',
+		description: 'Packages that tell you about a specific JS value.',
+	},
 };
 
 for (const [category, repos] of Object.entries(pkgs)) {
-    readme += `
+	readme += `
 ## ${meta[category].title}
 <details>
 <summary>${meta[category].description}</summary>
 <br />
 
 ${repos.map(({ repo, pkg: { name, description } = {} }) => {
-    return ` - ${name} - [![github][github]](https://github.com/${org}/${repo}) - [![npm][npm]](https://npmjs.com/${name}) - ${description}`;
+	return ` - ${name} - [![github][github]](https://github.com/${org}/${repo}) - [![npm][npm]](https://npmjs.com/${name}) - ${description}`;
 }).join('\n')}
 
 </details>
